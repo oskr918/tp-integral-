d7c05f5075cbbd13effb2709411417abe8693fb3
oscar <camamoblamientos@gmail.com>
2023-02-02 16:16:01 -0300

    conexion a db

--- integrador-backend/server.js
+++ integrador-backend/server.js
@@ -1,11 +1,20 @@
 const { json } = require('express');
 const express = require('express');
 const cors = require('cors')
+const connection = require('./conexion');
 
 const app = express();
 
 app.use(cors());
 app.use(json());
+
+ 
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+ 
+connection.end();
 
 app.listen(5000, ()=>{
     console.log('Servidor escuchando en el puerto 5000')
